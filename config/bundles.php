c3550d5830d3d2390b7c5d54bd496c2fad290638
Micka <micka75bup@gmail.com>
2026-02-06 11:22:58 +0100

    🔧 Add rate_limiter and verify_email
    Rate Limiter for registration and verify email when the user register

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
