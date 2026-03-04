acdc92f1ae8a88fe82be9619fb1dd38d64bfb7f9
Micka <micka75bup@gmail.com>
2026-03-04 09:47:55 +0100

    🔒️ Update lenght nickname
    From 1 to 3 minimim

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -63,7 +63,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(type: Types::STRING, length: 25, nullable: false)]
     #[NotBlank]
-    #[Length(min: 1, max: 20, maxMessage: 'user.nickname.length.max')]
+    #[Length(min: 3, max: 20, maxMessage: 'user.nickname.length.max')]
     private string $nickname;
 
     /**
