158aca83d42caca731016648aa113046e8b71ec3
Micka <micka75bup@gmail.com>
2026-03-16 18:26:49 +0100

    🎨 User locale
    Get either the user local or the requestLocale for the navigation

--- src/EventListener/LoginSuccessListener.php
+++ src/EventListener/LoginSuccessListener.php
@@ -39,8 +39,13 @@ final readonly class LoginSuccessListener
 
     private function redirectTo(LoginSuccessEvent $event): void
     {
+        /** @var User $user */
+        $user = $event->getUser();
+
+        $locale = $user->locale ?? $event->getRequest()->getLocale();
+
         $url = $this->urlGenerator->generate('app_dashboard', [
-            '_locale' => $event->getRequest()->getLocale(),
+            '_locale' => $locale,
         ]);
 
         $event->setResponse(new RedirectResponse($url));
