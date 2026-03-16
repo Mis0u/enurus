8f30f3e11470eac996fef0de7a43a56fc70c369d
Micka <micka75bup@gmail.com>
2026-03-16 18:21:31 +0100

    🧪 Edit fixtures
    Edit to add different langage

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -13,6 +13,16 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private const array USERS_ETHNIES = [
+        'en' => 'user-english@test.com',
+        'es' => 'user-spanish@test.com',
+        'de' => 'user-german@test.com',
+        'pt' => 'user-portuguese@test.com',
+        'pl' => 'user-polish@test.com',
+        'nl' => 'user-dutch@test.com',
+        'it' => 'user-italian@test.com',
+    ];
+
     public function __construct(
         private readonly UserPasswordHasherInterface $passwordHasher
     ) {
@@ -20,6 +30,20 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        foreach (self::USERS_ETHNIES as $key => $userEmail) {
+            $user = new User();
+            $lastLogin = now(\sprintf('+%d day +%d hours', 1, 1));
+            $user->email = $userEmail;
+            $user->nickname = \sprintf('user-country-%s', $key);
+            $user->createdBy = $user;
+            $user->locale = $key;
+            $user->lastLogin = $lastLogin;
+            $user->gender = 'male';
+            $password = $this->passwordHasher->hashPassword($user, 'pass_1234');
+            $user->password = $password;
+
+            $manager->persist($user);
+        }
         for ($i = 0; 40 > $i; ++$i) {
             $user = new User();
             $lastLogin = now(\sprintf('+%d day +%d hours', $i, $i));
