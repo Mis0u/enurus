3969146b31b483fd258e99bbd81bc27c570b7474
Micka <micka75bup@gmail.com>
2026-03-23 16:06:57 +0100

    🎨 Stimulus and LiveComponent
    Update the stimulus cause now the modal live in the liveComponent and add the component into the template

--- assets/controllers/search_modal_controller.js
+++ assets/controllers/search_modal_controller.js
@@ -1,17 +1,6 @@
 import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
-    showModal() {
-        document.getElementById('searchModal').classList.add('show');
-        document.getElementById('exerciseSearch').focus();
-    }
-
-    closeModal() {
-        document.getElementById('searchModal').classList.remove('show');
-    }
-
-    closeOnOverlay(event) {
-        if (event.target === document.getElementById('searchModal')) {
-            this.closeModal();
-        }
+    stop(event) {
+        event.stopPropagation();
     }
 }
