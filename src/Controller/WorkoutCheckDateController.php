b5b892b9bd4f1fa5c609215f6432636d03919fe4
Micka <micka75bup@gmail.com>
2026-05-12 14:45:33 +0200

    ✨ Add controllers
    Add controllers for the page workout. There are 2 others which are ExerciceBloc and WorkoutCheckDate which create the exerciceBlock and check the date for the input

---
+++ src/Controller/WorkoutCheckDateController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\WorkoutRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class WorkoutCheckDateController extends AbstractController
+{
+    #[Route('/workout/check-date', name: 'workout_check_date', methods: ['GET'])]
+    public function __invoke(Request $request, WorkoutRepository $workoutRepository, TranslatorInterface $translator): JsonResponse
+    {
+        $date = $request->query->get('date');
+
+        if (! $date) {
+            return $this->json([
+                'exists' => false,
+                'count' => 0,
+            ]);
+        }
+
+        $user = $this->getUser();
+        assert($user instanceof \App\Entity\User);
+
+        $start = new \DateTimeImmutable($date . ' 00:00:00');
+        $end = new \DateTimeImmutable($date . ' 23:59:59');
+
+        $count = $workoutRepository->countByUserAndDate($user, $start, $end);
+
+        return $this->json([
+            'exists' => 0 < $count,
+            'count' => $count,
+            'message' => $translator->trans('workout.check_date.message', [
+                'count' => $count,
+            ], 'navigation'),
+        ]);
+    }
+}
--- src/Controller/WorkoutController.php
+++ src/Controller/WorkoutController.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Workout;
 use App\Form\WorkoutType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class WorkoutController extends AbstractController
 {
@@ -25,12 +28,27 @@ class WorkoutController extends AbstractController
         'pl' => '/zapisz-trening',
     ], name: 'app_workout')]
     #[IsGranted('ROLE_USER')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $em, TranslatorInterface $translator): Response
     {
         /** @var User $user */
         $user = $this->getUser();
+
         $form = $this->createForm(WorkoutType::class);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Workout $workout */
+            $workout = $form->getData();
+            $workout->owner = $user;
+            $em->persist($workout);
+            $em->flush();
+            $this->addFlash('success', $translator->trans('workout.created', [], 'navigation'));
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        if ($form->isSubmitted() && ! $form->isValid()) {
+            return $this->redirectToRoute('app_workout');
+        }
 
         return $this->render('workout/index.html.twig', [
             'user' => $user,
