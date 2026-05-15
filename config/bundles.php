9d5eca0f63d7d01f22b25737fd802016ee1453fd
Micka <micka75bup@gmail.com>
2026-05-15 13:24:10 +0200

    ✨ Add knp_paginator
    Use to create page

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     AhmedBhs\DoctrineDoctor\DoctrineDoctorBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
