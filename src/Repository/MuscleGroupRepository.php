7a85a2b834e5249f3a7bbc197f74d4e0302b62f6
Micka <micka75bup@gmail.com>
2026-06-12 12:08:16 +0200

    ✨ Add new function
    New function in repo for DQL

--- src/Repository/MuscleGroupRepository.php
+++ src/Repository/MuscleGroupRepository.php
@@ -11,35 +11,22 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<MuscleGroup>
  */
-class MuscleGroupRepository extends ServiceEntityRepository
+final class MuscleGroupRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MuscleGroup::class);
     }
 
-    //    /**
-    //     * @return MuscleGroup[] Returns an array of MuscleGroup objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?MuscleGroup
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    /**
+     * @return list<MuscleGroup>
+     */
+    public function findAllOrderedByPosition(): array
+    {
+        /** @var list<MuscleGroup> */
+        return $this->createQueryBuilder('mg')
+            ->orderBy('mg.position', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
 }
