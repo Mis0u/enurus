eacdb567631003795636a1e747f132b1ef186fb2
Micka <micka75bup@gmail.com>
2026-06-22 11:06:02 +0200

    ✨ Service, Form and Controller
    Create controller for zdit exercise and the service and edit the form to handle create and edit

---
+++ src/Controller/ExerciseEditController.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Exercise;
+use App\Entity\User;
+use App\Form\ExerciseType;
+use App\Repository\MuscleGroupRepository;
+use App\Security\Voter\ExerciseVoter;
+use App\Service\Entity\ExerciseEditService;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(path: [
+    'fr' => '/bibliotheque/exercice/{id}/modifier',
+    'en' => '/library/exercise/{id}/edit',
+    'it' => '/biblioteca/esercizio/{id}/modifica',
+    'es' => '/biblioteca/ejercicio/{id}/editar',
+    'pt' => '/biblioteca/exercicio/{id}/editar',
+    'de' => '/bibliothek/uebung/{id}/bearbeiten',
+    'nl' => '/bibliotheek/oefening/{id}/bewerken',
+    'pl' => '/biblioteka/cwiczenie/{id}/edytuj',
+], name: 'app_exercise_edit', methods: ['GET', 'POST'])]
+final class ExerciseEditController extends AbstractController
+{
+    public function __construct(
+        private readonly ExerciseEditService $exerciseEditService,
+        private readonly MuscleGroupRepository $muscleGroupRepository,
+    ) {
+    }
+
+    public function __invoke(Request $request, Exercise $exercise): Response
+    {
+        $this->denyAccessUnlessGranted(ExerciseVoter::EDIT, $exercise);
+
+        $form = $this->createForm(ExerciseType::class, $exercise);
+        $form->get('muscles')->setData($exercise->exerciseMuscles);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->handleValidForm($form, $exercise);
+        }
+
+        return $this->renderEditForm($form, $exercise);
+    }
+
+    /**
+     * @param FormInterface<Exercise> $form
+     */
+    private function handleValidForm(FormInterface $form, Exercise $exercise): Response
+    {
+        $muscles = $form->get('muscles')->getData();
+
+        if (! $muscles instanceof Collection) {
+            throw new \LogicException('Expected muscles to be a Collection.');
+        }
+
+        if (! $this->exerciseEditService->hasPrimaryMuscle($muscles)) {
+            return $this->renderEditForm($form, $exercise);
+        }
+
+        $this->exerciseEditService->edit($exercise, $muscles);
+
+        $this->addFlash('success', 'exercise.edit.success');
+
+        return $this->redirectToRoute('app_exercise_history');
+    }
+
+    /**
+     * @param FormInterface<Exercise> $form
+     */
+    private function renderEditForm(FormInterface $form, Exercise $exercise): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->render('exercise/edit/index.html.twig', [
+            'form' => $form,
+            'exercise' => $exercise,
+            'gender' => $user->gender ?? 'male',
+            'muscleGroups' => $this->muscleGroupRepository->findAllOrderedByPosition(),
+            'user' => $user,
+        ]);
+    }
+}
