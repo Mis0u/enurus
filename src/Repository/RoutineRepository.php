c649d8bda773467c5a89821786624523e4a34a54
Micka <micka75bup@gmail.com>
2026-06-28 14:22:43 +0200

    ✨ Create for the moment to get the route
    Create the list controller but its minimal to get the strict minimum

--- src/Repository/RoutineRepository.php
+++ src/Repository/RoutineRepository.php
@@ -11,35 +11,10 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<Routine>
  */
-class RoutineRepository extends ServiceEntityRepository
+final class RoutineRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Routine::class);
     }
-
-    //    /**
-    //     * @return Routine[] Returns an array of Routine objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('r.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Routine
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
