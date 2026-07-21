bcd2294e60164ba4beca4ea848f2f2d55cdf08c0
Micka <micka75bup@gmail.com>
2026-07-21 12:19:55 +0200

    ✨ Add Knp pagination to routine list
    RoutineListController loaded every routine unconditionally. Repository
    now exposes a QueryBuilder and the controller paginates it (10/25/50
    selector), reusing the shared Knp pagination component.

--- src/Controller/Routine/RoutineListController.php
+++ src/Controller/Routine/RoutineListController.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller\Routine;
 
+use App\Entity\Routine;
 use App\Entity\User;
 use App\Repository\RoutineRepository;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -24,20 +28,34 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 ], name: 'app_routine_list', methods: ['GET'])]
 final class RoutineListController extends AbstractController
 {
+    private const int DISPLAY_LIMIT_BY_DEFAULT = 10;
+
+    private const array DISPLAY_LIMIT_ALLOWED = [10, 25, 50];
+
     public function __construct(
         private readonly RoutineRepository $routineRepository,
     ) {
     }
 
-    public function __invoke(): Response
+    public function __invoke(Request $request, PaginatorInterface $paginator): Response
     {
         /** @var User $user */
         $user = $this->getUser();
-        $routines = $this->routineRepository->findByOwnerOrderedByDate($user);
+
+        $limit = $request->query->getInt('limit', self::DISPLAY_LIMIT_BY_DEFAULT);
+        $limit = in_array($limit, self::DISPLAY_LIMIT_ALLOWED, true) ? $limit : self::DISPLAY_LIMIT_BY_DEFAULT;
+
+        /** @var PaginationInterface<int, Routine> $pagination */
+        $pagination = $paginator->paginate(
+            $this->routineRepository->findByOwnerOrderedByDate($user),
+            $request->query->getInt('page', 1),
+            $limit
+        );
 
         return $this->render('routine/list/index.html.twig', [
-            'routines' => $routines,
-            'count' => count($routines),
+            'pagination' => $pagination,
+            'count' => $pagination->getTotalItemCount(),
+            'limitAllowed' => self::DISPLAY_LIMIT_ALLOWED,
         ]);
     }
 }
